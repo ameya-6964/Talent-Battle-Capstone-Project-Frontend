16029a098c6d716a45c23c2303cb29974ba1bf24
ameya-6964 <belvalkar7@gmail.com>
2023-01-17 22:40:51 +0530

    Completed Home Page And Installed React Router

--- src/App.js
+++ src/App.js
@@ -1,10 +1,33 @@
+import React from "react";
+import data from "./data";
+
 function App() {
   return (
     <div>
       <header>
         <a href="/"> Shop City </a>
       </header>
-      <main>List Products Here</main>
+      <main>
+        <h1> Featured Products </h1>
+        <div className="products">
+          {data.products.map((product) => (
+            <div className="product" key={product.slug}>
+              <a href={`/product/${product.slug}`}>
+                <img src={product.image} alt={product.name} />
+              </a>
+              <div className="product-info">
+                <a href={`/product/${product.slug}`}>
+                  <p>{product.name}</p>
+                </a>
+                <p>
+                  <strong>{product.price}</strong>
+                </p>
+                <button>Add To Cart 🛒</button>
+              </div>
+            </div>
+          ))}
+        </div>
+      </main>
     </div>
   );
 }
---
+++ src/data.js
@@ -0,0 +1,55 @@
+const data = {
+  products: [
+    {
+      name: "Nike Slim Shirt",
+      slug: "nike-slim-shirt",
+      category: "Shirts",
+      image: "/images/nikeTshirt.jpg",
+      price: "1000 INR",
+      countInStock: 10,
+      //? 679 x 829 px
+      brand: "Nike",
+      rating: 4.5,
+      numReviews: 10,
+      description: "High Quality Shirt",
+    },
+    {
+      name: "Adidas Trendy Hoody",
+      slug: "adidas-trendy-hoody",
+      category: "Hoodies",
+      image: "/images/adidasHoody.jpg",
+      price: "1200 INR",
+      countInStock: 10,
+      brand: "Adidas",
+      rating: 4,
+      numReviews: 5,
+      description: "High Quality Hoodies",
+    },
+    {
+      name: "Nike Sports Shoes",
+      slug: "nike-sports-shoes",
+      category: "Shoes",
+      image: "/images/jordans.jpg",
+      price: "2000 INR",
+      countInStock: 4,
+      brand: "Nike",
+      rating: 4.5,
+      numReviews: 10,
+      description: "High Quality Sports Shoes",
+    },
+    {
+      name: "Puma Polo Tshirt",
+      slug: "puma-polo-tshirt",
+      category: "Shirts",
+      image: "/images/polo.jpg",
+      price: "1200 INR",
+      countInStock: 10,
+      brand: "Puma",
+      rating: 4.5,
+      numReviews: 10,
+      description: "Classy Polo Shirt",
+    },
+  ],
+};
+
+export default data;
