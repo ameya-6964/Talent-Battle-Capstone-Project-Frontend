46b870a5f144d7ebe6b2d49ec4fb7d941bd9c18a
ameya-6964 <belvalkar7@gmail.com>
2023-01-18 22:17:23 +0530

    Basic Styling For Top Wear

--- src/Screens/TopWear.jsx
+++ src/Screens/TopWear.jsx
@@ -1,10 +1,28 @@
 import React from "react";
+import data from "../data";
+/* import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import { Link } from "react-router-dom";
+import Card from "react-bootstrap/Card"; */
 
 const TopWear = () => {
   return (
-    <div className="site-container">
-      <h1>I Am Top Wear</h1>
-    </div>
+    <main>
+      <h1 className="text-center"> Featured Top Wear </h1>
+      <div className="products">
+        {data.topWears.map((topwear) => (
+          <div className="product" key={topwear.slug}>
+            <a href={`/TopWear/${topwear.slug}`}>
+              <img src={topwear.image} alt={topwear.name} />
+            </a>
+            <div className="product-info">
+              <p>{topwear.name}</p>
+              <p>{topwear.price}</p>
+            </div>
+          </div>
+        ))}
+      </div>
+    </main>
   );
 };
 
