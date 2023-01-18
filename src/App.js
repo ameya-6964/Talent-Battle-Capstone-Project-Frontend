d873a5f9b8d0b2fedb7792506891a36f83816ae9
ameya-6964 <belvalkar7@gmail.com>
2023-01-18 11:29:26 +0530

    Edited Components

--- src/App.js
+++ src/App.js
@@ -1,36 +1,24 @@
 import React from "react";
-
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import HomeScreen from "./components/HomeScreen";
 import ProductScreen from "./components/ProductScreen";
-import Navbar from "react-bootstrap/Navbar";
 import Container from "react-bootstrap/Container";
-import { LinkContainer } from "react-router-bootstrap";
 import Footer from "./components/Footer";
+import Navigation from "./components/Navigation";
 
 function App() {
   return (
     <BrowserRouter>
-      <div>
-        <header>
-          <Navbar bg="dark" variant="dark">
-            <Container>
-              <LinkContainer to="/">
-                <Navbar.Brand>Shop City 🛍️</Navbar.Brand>
-              </LinkContainer>
-            </Container>
-          </Navbar>
-        </header>
-        <main>
-          <Container>
-            <Routes>
-              <Route path="/products/:slug" element={<ProductScreen />}></Route>
-              <Route path="/" element={<HomeScreen />}></Route>
-            </Routes>
-          </Container>
-        </main>
-        <Footer />
-      </div>
+      <Navigation />
+      <main>
+        <Container>
+          <Routes>
+            <Route path="/products/:slug" element={<ProductScreen />}></Route>
+            <Route path="/" element={<HomeScreen />}></Route>
+          </Routes>
+        </Container>
+      </main>
+      <Footer />
     </BrowserRouter>
   );
 }
---
+++ src/components/Navigation.jsx
@@ -0,0 +1,39 @@
+import React from "react";
+//import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Navbar from "react-bootstrap/Navbar";
+import Container from "react-bootstrap/Container";
+import Nav from "react-bootstrap/Nav";
+import { LinkContainer } from "react-router-bootstrap";
+
+const Navigation = () => {
+  return (
+    <header>
+      <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
+        <Container>
+          <LinkContainer to="/">
+            <Navbar.Brand>Shop City 🛍️</Navbar.Brand>
+          </LinkContainer>
+          <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+          <Navbar.Collapse id="responsive-navbar-nav">
+            <Nav className="me-auto">
+              <LinkContainer to="/">
+                <Nav.Link>Top Wear</Nav.Link>
+              </LinkContainer>
+              <LinkContainer to="/">
+                <Nav.Link>Bottom Wear</Nav.Link>
+              </LinkContainer>
+              <LinkContainer to="/">
+                <Nav.Link>Sports</Nav.Link>
+              </LinkContainer>
+              <LinkContainer to="/">
+                <Nav.Link>Electronics</Nav.Link>
+              </LinkContainer>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+    </header>
+  );
+};
+
+export default Navigation;
